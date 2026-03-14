d4e175e3e915fe8c45c5f42d113d06733255cf0d
bijenchipalu <bijenchipalu@gmail.com>
2026-03-14 18:29:08 +0545

    Add files via upload

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ tenuer = st.slider("Month Subscribed",1,60)
 if st.button("Predict"):
     prediction = model.predict([[age,1,2,15.99,tenure,120,3,1,2]])
 
-    if prediction[0] ==1;
+    if prediction[0] == 1:
         st.error("User likely to churn")
     else:
         st.success("User Likely to Stay")
