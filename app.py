55f0a77f3d4f871d2db7aa860b9bf7d7542d1531
bijenchipalu <bijenchipalu@gmail.com>
2026-03-14 18:30:40 +0545

    Add files via upload

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ model = pickle.load(open("churn_model.pkl","rb"))
 st.title("Netflix Churn Prediction")
 
 age = st.slider("Age",18,70)
-tenuer = st.slider("Month Subscribed",1,60)
+tenure = st.slider("Month Subscribed",1,60)
 
 if st.button("Predict"):
     prediction = model.predict([[age,1,2,15.99,tenure,120,3,1,2]])
